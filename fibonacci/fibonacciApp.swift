15dbffc10158574d861c20387f6fd42b29cda3e9
Collinw24 <collinwiggins@gmail.com>
2025-12-19 00:46:22 -0500

    Initial Commit

---
+++ fibonacci/fibonacciApp.swift
@@ -0,0 +1,17 @@
+//
+//  fibonacciApp.swift
+//  fibonacci
+//
+//  Created by collin on 12/19/25.
+//
+
+import SwiftUI
+
+@main
+struct fibonacciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
